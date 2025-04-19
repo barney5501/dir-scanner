b38a55afc6be4240a98a36156491c641c8f26f3f
Itay <Itaybn100@gmail.com>
2025-04-19 15:46:34 +0300

    Added base dirscanner visual app built with streamlit

---
+++ dirscanner_app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data
+def load_data():
+    file_tree = pd.read_csv("root.csv")[["filename", "parent", "type", "size"]]
+    return file_tree
+
+
+def main():
+    print("run")
+    st.title("Dir_ Mapper")
+    st.write("Explore your folders")
+
+    file_tree = load_data()
+    if "root_folder" not in st.session_state:
+        st.session_state.root_folder = file_tree.iloc[-1]["filename"]
+    if "current_dir" not in st.session_state:
+        st.session_state.current_dir = st.session_state.root_folder
+
+    # filter to current folder
+    current_file_tree = file_tree.loc[
+        file_tree["parent"] == st.session_state.current_dir
+    ]
+
+    st.text(f"{st.session_state.current_dir}")
+    st.bar_chart(
+        data=current_file_tree.sort_values(by="size", ascending=False),
+        x="filename",
+        y="size",
+        horizontal=True,
+        height=500,
+    )
+
+    option = st.selectbox(
+        "subfolder",
+        current_file_tree.loc[current_file_tree["type"] == "folder"],
+        index=None,
+        placeholder="Select subfolder",
+    )
+
+    if option:
+        st.session_state.current_dir = option
+
+
+if __name__ == "__main__":
+    main()
